97561cf86f5b72bdb00cf1931a4026b30e1d858c
raozhaizhu <raozhaizhu@gmail.com>
2025-01-10 16:39:30 +0800

    Update vite.config.js with new base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/country/',
+    base: '/countryPage/', // 将 'new-repo-name' 替换为您的新仓库名
 });
 
